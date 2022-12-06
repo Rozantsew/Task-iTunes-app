2890712f311a2187fd4733646e524640d30c7d13
Michał <mykhailorozantsev@gmail.com>
2022-12-06 23:52:08 +0100

    Initial Commit

---
+++ Task iTunes App/Task_iTunes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_iTunes_AppApp.swift
+//  Task iTunes App
+//
+//  Created by Mykhailo Rozantsev on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Task_iTunes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
